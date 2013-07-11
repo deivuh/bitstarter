6199b31acf03000558f79e7359e23ee05b315e5e
Ubuntu <ubuntu@ip-172-31-37-131.us-west-2.compute.internal>
2013-07-11 04:32:58 +0000

    web.js changes

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync("index.html", encoding='UTF-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    console.log(fs.readFile('index.html'));
+
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+  console.log(contents);
   console.log("Listening on " + port);
 });
