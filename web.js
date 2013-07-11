02154f3c94b9f5d7f64e17a720877f526d95d3d7
Ubuntu <ubuntu@ip-172-31-37-131.us-west-2.compute.internal>
2013-07-11 04:38:15 +0000

    web.js changes

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var contents = fs.readFileSync("index.html", encoding='UTF-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
 //    console.log(fs.readFile('index.html'));
     response.send(contents);
 
